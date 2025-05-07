c174892649a84bd2a8b5fdcec7f666d51ad051eb
Ceriumin <barylagracjan@icloud.com>
2025-05-07 23:08:44 +0100

    fix(auth): amplify file misconfiguration

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './src/navigation/StackNavigator';
 import {name as appName} from './app.json';
 import { Amplify } from 'aws-amplify';
-import awsconfig from '/amplifyconfiguration.json';
+import awsconfig from './aws-exports';
 
 Amplify.configure(awsconfig);
 AppRegistry.registerComponent(appName, () => App);
