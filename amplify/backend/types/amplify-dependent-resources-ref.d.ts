fd601a20a84ca0b2d74937f18d2b6b9482487a2f
Ceriumin <barylagracjan@icloud.com>
2025-05-07 17:22:53 +0100

    chore(initialise AWS Amplify):

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "calder": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
--- index.js
+++ index.js
@@ -5,5 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './src/navigation/StackNavigator';
 import {name as appName} from './app.json';
+import { Amplify } from 'aws-amplify';
+import awsconfig from '/amplifyconfiguration.json';
 
+Amplify.configure(awsconfig);
 AppRegistry.registerComponent(appName, () => App);
